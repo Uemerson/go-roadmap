04c5e85d3bc408cc0b31af42d9d8266b01d93cd4
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-01-19 23:10:22 -0300

    feat: add go run /home/uemerson/projects/go-roadmap/learn-the-basics/errors-panic-recover/examples/fmt-errorf/main.go code example

---
+++ learn-the-basics/errors-panic-recover/examples/fmt-errorf/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("can't divide '%d' by zero", a)
+	}
+	return a / b, nil
+}
+
+func main() {
+	value, err := Divide(10, 0)
+
+	if err == nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(value)
+	}
+}
