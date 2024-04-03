269c70624563bb6487b3e472f4b749737663dd01
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-04-02 22:47:05 -0300

    feat: add race-condition code example

---
+++ going-deeper/mutex/examples/race-condition/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var counter = 0
+
+func increment(wg *sync.WaitGroup) {
+	counter++
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	expectedCounter := 1000
+
+	for i := 0; i < expectedCounter; i++ {
+		wg.Add(1)
+		go increment(&wg)
+	}
+
+	wg.Wait()
+	fmt.Println("Expected Counter:", expectedCounter)
+	fmt.Println("Actual Counter:", counter)
+	// Check for race condition
+	if expectedCounter != counter {
+		fmt.Println("Race condition detected!")
+	} else {
+		fmt.Println("No race condition detected.")
+	}
+}
